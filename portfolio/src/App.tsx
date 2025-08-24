fbe3c2e2d9fc6cb1da1db1fa49bd49c3e4eb76fb
Nicksternick <nicholasd1124@yahoo.com>
2025-08-24 12:13:23 -0400

    Push

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,10 +1,50 @@
+import {
+    BrowserRouter,
+    Link,
+    Route,
+    Routes
+} from 'react-router-dom'
 import '../portfolio.css'
+
+function Home() {
+    return <h1>Home Page</h1>;
+}
+
+function About() {
+    return <h1>About Page</h1>;
+}
+
+function Contact() {
+    return <h1>Contact Page</h1>;
+}
 
 function App() {
     return (
         <>
-            <button className='button is-primary'>Zing</button>
-            <div>Hello World</div>
+            <BrowserRouter>
+                <div className="section">
+                    <nav className="navbar has-background-primary">
+                        <div className="navbar-menu">
+                            <div className="navbar-start">
+                                <Link to="/" className="navbar-item">
+                                    Home
+                                </Link>
+                                <Link to="/about" className="navbar-item">
+                                    About
+                                </Link>
+                                <Link to="/contact" className="navbar-item">
+                                    Contact
+                                </Link>
+                            </div>
+                        </div>
+                    </nav>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </>
     )
 }
