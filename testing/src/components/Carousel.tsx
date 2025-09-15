50d099fa2c39e4abf6cafb1f133c64dd1f90738b
Nicksternick <111940777+Nicksternick@users.noreply.github.com>
2025-09-15 10:48:01 -0400

    push carousel test

---
+++ testing/src/components/Carousel.tsx
@@ -0,0 +1,14 @@
+import '../css/carousel.css'
+
+export default function Carousel() {
+    return (
+        <div className="c-body m-2 has-background-primary">
+            <figure className='c-image has-text-centered 
+            is-flex
+            is-align-content-center
+            is-justify-content-center'>
+                <img src="https://placehold.co/400" alt="" />
+            </figure>
+        </div>
+    )
+}
