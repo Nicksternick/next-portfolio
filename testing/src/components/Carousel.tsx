8c980291578605e3f55a2315417cbc41947f94a8
Nicksternick <111940777+Nicksternick@users.noreply.github.com>
2025-09-15 21:52:29 -0400

    Push

--- testing/src/components/Carousel.tsx
+++ testing/src/components/Carousel.tsx
@@ -2,13 +2,20 @@ import '../css/carousel.css'
 
 export default function Carousel() {
     return (
-        <div className="c-body m-2 has-background-primary">
-            <figure className='c-image has-text-centered 
-            is-flex
-            is-align-content-center
-            is-justify-content-center'>
-                <img src="https://placehold.co/400" alt="" />
-            </figure>
-        </div>
+        <>
+            <div className='c-wrapper'>
+                <button className='c-arrow c-prev'>&#8592;</button>
+                <div className="c-body m-2 has-background-primary">
+                    <figure className='c-image has-text-centered 
+                    is-flex
+                    is-align-content-center
+                    is-justify-content-center'>
+                        <img src="https://placehold.co/1280x800" alt="" />
+                    </figure>
+                </div>
+                <button className='c-arrow c-next'>&#8594;</button>
+            </div>
+
+        </>
     )
 }
